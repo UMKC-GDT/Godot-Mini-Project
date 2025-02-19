85fc959915a7859a9528a9bbf9bd2538bb0dc959
umkcVR <cgch6w@umsystem.edu>
2025-02-19 17:13:21 -0600

    made 'em do a little dance

--- pacman_sprite.gd
+++ pacman_sprite.gd
@@ -3,12 +3,17 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("hello")
-	#pass # Replace with function body.
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	print("hello")
-	#pass
+func _process(delta):
+	pass
 	
+func _input(event):
+	if event.is_action_pressed("space_bar"):
+		scale.y = -4
+		if(skew<10):
+			skew = 10
+		elif(skew > -10):
+			skew = -10
