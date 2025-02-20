df615782de5f5f4048197451d81a682d387e30ca
Trevor Norton <nortrevor02@gmail.com>
2025-02-20 12:29:49 -0600

    Implemented Base Character Controller Scene
    Also added in support for animation later down the line.

--- Scenes/character_controller.gd
+++ Scenes/character_controller.gd
@@ -1,11 +1,41 @@
-extends Node2D
+extends CharacterBody2D
 
+@export var speed: int = 100
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta):
+	velocity = Vector2.ZERO # The player's movement vector.
+	
+#region Input Management
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	
+#region Animation Management
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "mouthClosed"
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "mouthOpen"
+	else:
+		$AnimatedSprite2D.stop()
+	$AnimatedSprite2D.flip_v = velocity.y > 0
+#endregion
+	
+#region Movement Management
+	if velocity.length() > 0:
+		# look_at(global_position * velocity) #oh this fucked up fucked up
+		velocity = velocity.normalized() * speed
+	position += velocity * delta
+	move_and_slide()
+#endregion
